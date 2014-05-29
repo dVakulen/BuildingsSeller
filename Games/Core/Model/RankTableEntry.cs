b6004d74c7c4bd04ff553d3d8af30f2b978d121f
777Eternal777 <dimitriu777@gmail.com>
2014-05-29 18:53:10 +0300

    asxa

---
+++ Games/Core/Model/RankTableEntry.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DragDropPhoneApp.Model
+{
+    using System.Data.Linq.Mapping;
+
+  
+        [Table]
+        public class RankTableEntry
+        {
+            #region Fields
+
+            private Guid id;
+
+            #endregion
+
+            #region Public Properties
+
+            [Column(IsPrimaryKey = true, IsDbGenerated = true, CanBeNull = false, AutoSync = AutoSync.OnInsert)]
+            public Guid Id
+            {
+                get
+                {
+                    return this.id;
+                }
+
+                set
+                {
+                    this.id = value;
+                }
+            }
+
+
+            [Column]
+            public string UserName { get; set; }
+            [Column]
+            public string TimePassed { get; set; }
+            #endregion
+        }
+   
+}
---
+++ Games/DragDropPhoneApp/IoC/ServiceLocator.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DragDropPhoneApp.IoC
+{
+    using System.Windows;
+
+    using DragDropPhoneApp.ViewModel;
+
+    using GalaSoft.MvvmLight.Ioc;
+    using GalaSoft.MvvmLight.Messaging;
+
+    using Microsoft.Practices.ServiceLocation;
+
+    public class ViewModelLocator
+    {
+        /// <summary>
+        /// Initializes a new instance of the ViewModelLocator class.
+        /// </summary>
+        public ViewModelLocator()
+        {
+            ServiceLocator.SetLocatorProvider(() => SimpleIoc.Default);
+
+            ////if (ViewModelBase.IsInDesignModeStatic)
+            ////{
+            ////    // Create design time view services and models
+            ////    SimpleIoc.Default.Register<IDataService, DesignDataService>();
+            ////}
+            ////else
+            ////{
+            ////    // Create run time view services and models
+            ////    SimpleIoc.Default.Register<IDataService, DataService>();
+            ////}
+
+            SimpleIoc.Default.Register<MainViewModel>();
+        }
+
+        /// <summary>
+        /// Gets the main view model.
+        /// </summary>
+        /// <value>
+        /// The main view model.
+        /// </value>
+        public MainViewModel MainViewModel
+        {
+            get
+            {
+                return ServiceLocator.Current.GetInstance<MainViewModel>();
+            }
+        }
+
+        /// <summary>
+        /// Cleanups this instance.
+        /// </summary>
+        public static void Cleanup()
+        {
+            // TODO Clear the ViewModels
+            var viewModelLocator = (ViewModelLocator)Application.Current.Resources["Locator"];
+            viewModelLocator.MainViewModel.Cleanup();
+
+            Messenger.Reset();
+        }
+    }
+}
